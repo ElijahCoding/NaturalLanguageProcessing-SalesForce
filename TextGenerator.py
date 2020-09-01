82f5e86c5f4fde2100106abac265abb59403a208
ElijahCoding <cuiyeqing960904@gmail.com>
2020-09-02 00:29:23 +0300

    chunking data

--- TextGenerator.py
+++ TextGenerator.py
@@ -24,3 +24,29 @@ for char, _ in zip(char2idx, range(20)):
 print('...\n')
 
 print('{} ----> characters mapped to int ----> {}'.format(repr(text[:13]), text_as_int[:13]))
+
+seq_length = 100
+examples_per_epoch = len(text) // (seq_length + 1)
+
+char_dataset = tf.data.Dataset.from_tensor_slices(text_as_int)
+
+# for i in char_dataset.take(5):
+#     print(idx2char[i.numpy()])
+
+sequences = char_dataset.batch(seq_length+1, drop_remainder=True)
+
+# for item in sequences.take(5):
+#     print(repr(''.join(idx2char[item.numpy()])))
+
+def split_input_target(chunk):
+    input_text = chunk[:-1]
+    target_text = chunk[1:]
+    return input_text, target_text
+
+dataset = sequences.map(split_input_target)
+
+# for input_example, target_example in dataset.take(1):
+#     print('input data: ', repr(''.join(idx2char[input_example.numpy()])))
+#     print('\n')
+#     print('taregt data: ', repr(''.join(idx2char[target_example.numpy()])))
+
