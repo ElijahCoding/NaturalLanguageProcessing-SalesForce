c9c4115f124a1b037f5d3f2505e242f63c8ac38b
yeqing <yeqing@yeqings-Mac-mini.local>
2020-09-01 15:49:30 +0300

    model defined

--- WordEmbeddings.py
+++ WordEmbeddings.py
@@ -20,4 +20,18 @@ import tensorflow_datasets as tfds
 )
 
 encoder = info.features['text'].encoder
-print(encoder.subwords[:20])
+# print(encoder.subwords[:20])
+
+padded_shapes = ([None], ())
+
+train_batches = train_data.shuffle(1000).padded_batches(10, padded_shapes=padded_shapes)
+test_batches = test_data.shuffle(1000).padded_batches(10, padded_shapes=padded_shapes)
+
+embedding_dim = 16
+model = keras.Sequential([
+    layers.Embedding(encoder.vocab_size, embedding_dim),
+    layers.GlobalAvgPool1D(),
+    layers.Dense(1, activation='sigmoid')
+])
+
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
