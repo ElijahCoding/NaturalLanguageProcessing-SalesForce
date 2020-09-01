44f754833fad9634f89935c152570640279d4aee
ElijahCoding <cuiyeqing960904@gmail.com>
2020-09-01 19:09:49 +0300

    model trained

--- TextClassificationRNN.py
+++ TextClassificationRNN.py
@@ -3,7 +3,30 @@ import tensorflow_datasets as tfds
 
 dataset, info = tfds.load('imdb_reviews/subwords8k', with_info=True, as_supervised=True)
 
-train_data, test_data = dataset['train'], dataset['test']
+train_dataset, test_dataset = dataset['train'], dataset['test']
 
 encoder = info.features['text'].encoder
 
+BUFFER_SIZE = 10000
+BATCH_SIZE = 64
+
+padded_shapes = ([None], ())
+
+train_dataset = train_dataset.shuffle(BUFFER_SIZE).padded_batch(BATCH_SIZE, padded_shapes=padded_shapes)
+test_dataset = test_dataset.padded_batch(BATCH_SIZE, padded_shapes=padded_shapes)
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(encoder.vocab_size, 64),
+    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64, return_sequences=True)),
+    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(32)),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+model.compile(
+    loss='binary_crossentropy', 
+    optimizer=tf.keras.optimizers.Adam(lr=1e-4),
+    metrics=['accuracy']
+)
+
+history = model.fit(train_dataset, epochs=20, validation_data=test_dataset, validation_steps=30)
