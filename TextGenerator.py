0ec9c080f8690784cbd8c44ffa9f2098da1c527e
ElijahCoding <cuiyeqing960904@gmail.com>
2020-09-01 20:48:09 +0300

    data preprocessed

--- TextGenerator.py
+++ TextGenerator.py
@@ -6,4 +6,14 @@ path_to_file = tf.keras.utils.get_file('shakespeare.txt', 'https://storage.googl
 
 text = open(path_to_file, 'rb').read().decode(encoding='utf-8')
 
-print(len(text))
+# print(len(text))
+
+vocab = sorted(set(text))
+# print(len(vocab))
+
+char2idx = {u:i for i, u in enumerate(vocab)}
+idx2char = np.array(vocab)
+
+text_as_int = np.array([char2idx[char] for char in text])
+
+print(text_as_int)
