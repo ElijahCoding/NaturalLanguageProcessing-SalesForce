60eec2de459ddedbb91f4563b1dcd1176b720889
ElijahCoding <cuiyeqing960904@gmail.com>
2020-09-01 20:39:53 +0300

    shakespear imported

---
+++ TextGenerator.py
@@ -0,0 +1,9 @@
+import os
+import tensorflow as tf
+import numpy as np
+
+path_to_file = tf.keras.utils.get_file('shakespeare.txt', 'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt')
+
+text = open(path_to_file, 'rb').read().decode(encoding='utf-8')
+
+print(len(text))
