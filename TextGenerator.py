04a68d6a95875e6f30c2777fbf77148373483cc6
ElijahCoding <cuiyeqing960904@gmail.com>
2020-09-02 00:51:20 +0300

    looping chunks

--- TextGenerator.py
+++ TextGenerator.py
@@ -2,7 +2,8 @@ import os
 import tensorflow as tf
 import numpy as np
 
-path_to_file = tf.keras.utils.get_file('shakespeare.txt', 'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt')
+path_to_file = tf.keras.utils.get_file('shakespeare.txt',
+                                       'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt')
 
 text = open(path_to_file, 'rb').read().decode(encoding='utf-8')
 
@@ -11,7 +12,7 @@ text = open(path_to_file, 'rb').read().decode(encoding='utf-8')
 vocab = sorted(set(text))
 # print(len(vocab))
 
-char2idx = {u:i for i, u in enumerate(vocab)}
+char2idx = {u: i for i, u in enumerate(vocab)}
 idx2char = np.array(vocab)
 
 text_as_int = np.array([char2idx[char] for char in text])
@@ -33,7 +34,8 @@ char_dataset = tf.data.Dataset.from_tensor_slices(text_as_int)
 # for i in char_dataset.take(5):
 #     print(idx2char[i.numpy()])
 
-sequences = char_dataset.batch(seq_length+1, drop_remainder=True)
+sequences = char_dataset.batch(seq_length + 1, drop_remainder=True)
+
 
 # for item in sequences.take(5):
 #     print(repr(''.join(idx2char[item.numpy()])))
@@ -43,10 +45,14 @@ def split_input_target(chunk):
     target_text = chunk[1:]
     return input_text, target_text
 
+
 dataset = sequences.map(split_input_target)
 
-# for input_example, target_example in dataset.take(1):
-#     print('input data: ', repr(''.join(idx2char[input_example.numpy()])))
-#     print('\n')
-#     print('taregt data: ', repr(''.join(idx2char[target_example.numpy()])))
+for input_example, target_example in dataset.take(1):
+    print('Input data: ', repr(''.join(idx2char[input_example.numpy()])))
+    print('Target data:', repr(''.join(idx2char[target_example.numpy()])))
 
+for i, (input_idx, target_idx) in enumerate(zip(input_example[:5], target_example[:5])):
+    print("Step {:4d}".format(i))
+    print("  input: {} ({:s})".format(input_idx, repr(idx2char[input_idx])))
+    print("  expected output: {} ({:s})".format(target_idx, repr(idx2char[target_idx])))
