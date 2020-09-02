8141eb29ec20a72b4d64e08f51e084fbb4a2e50a
ElijahCoding <cuiyeqing960904@gmail.com>
2020-09-02 22:45:21 +0300

    generate text

--- TextGenerator.py
+++ TextGenerator.py
@@ -108,3 +108,31 @@ history = model.fit(dataset, epochs=EPOCHS, callbacks=[checkpoint_callback])
 # model.load_weights(tf.train.latest_checkpoint(checkpoint_dir))
 # model.build(tf.TensorShape([1, None]))
 # model.summary()
+
+def generate_text(model, start_string):
+    num_generate = 1000
+    input_eval = [char2idx[s] for s in start_string]
+    input_eval = tf.expand_dims(input_eval, 0)
+
+    text_generated = []
+    temperature = 1.0
+
+    model.reset_states()
+    for i in range(num_generate):
+        predictions = model(input_eval)
+        # remove the batch dimension
+        predictions = tf.squeeze(predictions, 0)
+
+        # using a categorical distribution to predict the character returned by the model
+        predictions = predictions / temperature
+        predicted_id = tf.random.categorical(predictions, num_samples=1)[-1, 0].numpy()
+
+        # We pass the predicted character as the next input to the model
+        # along with the previous hidden state
+        input_eval = tf.expand_dims([predicted_id], 0)
+
+        text_generated.append(idx2char[predicted_id])
+
+    return (start_string + ''.join(text_generated))
+
+print(generate_text(model, start_string=u"ROMEO: "))
