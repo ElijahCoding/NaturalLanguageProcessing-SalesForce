ec98c3dc453182095368e5286529005e3157b651
ElijahCoding <cuiyeqing960904@gmail.com>
2020-09-07 23:23:23 +0300

    tokenize sentence and stem words

--- NLP-Chatbot-Pytorch/nltk_utils.py
+++ NLP-Chatbot-Pytorch/nltk_utils.py
@@ -1,12 +1,26 @@
 import nltk
 
-nltk.download('punkt')
+# nltk.download('punkt')
+
+from nltk.stem.porter import PorterStemmer
+
+stemmer = PorterStemmer()
 
 def tokenize(sentence):
     return nltk.word_tokenize(sentence)
 
 def stem(word):
-    pass
+    return stemmer.stem(word.lower())
 
 def bag_of_words(tokenized_sentence, all_words):
     pass
+
+# example = 'How long does shipping take?'
+# print(example)
+#
+# example = tokenize(example)
+# print(example)
+
+# words = ['organize', 'organizes', 'organizing']
+# stemmed_words = [stem(word) for word in words]
+# print(stemmed_words)
