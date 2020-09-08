eb9bc560e113e4e247cebc2b204bca614da36ace
ElijahCoding <cuiyeqing960904@gmail.com>
2020-09-08 12:43:31 +0300

    bag of words func

--- NLP-Chatbot-Pytorch/train.py
+++ NLP-Chatbot-Pytorch/train.py
@@ -1,5 +1,7 @@
 import json
 from nltk_utils import tokenize, stem, bag_of_words
+import numpy as np
+
 
 with open('intents.json', 'r') as f:
     intents = json.load(f)
@@ -18,4 +20,19 @@ for intent in intents['intents']:
         xy.append((w, tag))
 
 ignore_words = ['?', '!', '.', ',']
-print(all_words)
+all_words = [stem(w) for w in all_words if w not in ignore_words]
+all_words = sorted(set(all_words))
+
+tags = sorted(set(tags))
+
+X_train = []
+y_train = []
+for (pattern_sentence, tag) in xy:
+    bag = bag_of_words(pattern_sentence, all_words)
+    X_train.append(bag)
+
+    label = tags.index(tag)
+    y_train.append(label) # CrossEntropyLoss
+
+X_train = np.array(X_train)
+y_train = np.array(y_train)
