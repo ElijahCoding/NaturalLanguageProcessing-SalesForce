8ae33bc8037f299f5ffef68b7a3ead45c63f9bc3
ElijahCoding <cuiyeqing960904@gmail.com>
2020-09-10 12:33:13 +0300

    visualizing img

--- HandMadeNeuralNetwork/nn2.py
+++ HandMadeNeuralNetwork/nn2.py
@@ -6,3 +6,17 @@ import os
 def load_data():
     with open('dataset/train-labels-idx1-ubyte', 'rb') as labels:
         magic, n = struct.unpack('>II', labels.read(8))
+        train_labels = np.fromfile(labels, dtype=np.uint8)
+    with open('dataset/train-images.idx3-ubyte', 'rb') as imgs:
+        magic, num, nrows, ncols = struct.unpack('>IIII', imgs.read(16))
+        train_images = np.fromfile(imgs, dtype=np.uint8).reshape(num, 784)
+    with open('dataset/t10k-labels.idx1-ubyte', 'rb') as labels:
+        magic, n = struct.unpack('>II', labels.read(8))
+        test_labels = np.fromfile(labels, dtype=np.uint8)
+    with open('dataset/t10k-images.idx3-ubyte', 'rb') as imgs:
+        magic, num, nrows, ncols = struct.unpack('>IIII', imgs.read(16))
+        test_images = np.fromfile(imgs, dtype=np.uint8).reshape(num, 784)
+    return train_images, train_labels, test_images, test_labels
+
+def visualize_data(img_array, label_array):
+    
