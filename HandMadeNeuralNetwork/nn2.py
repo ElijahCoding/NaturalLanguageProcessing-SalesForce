0b817f617d3630e58bbd040734e17b26d786a6a9
ElijahCoding <cuiyeqing960904@gmail.com>
2020-09-10 12:55:10 +0300

    visualize data

--- HandMadeNeuralNetwork/nn2.py
+++ HandMadeNeuralNetwork/nn2.py
@@ -7,16 +7,27 @@ def load_data():
     with open('dataset/train-labels-idx1-ubyte', 'rb') as labels:
         magic, n = struct.unpack('>II', labels.read(8))
         train_labels = np.fromfile(labels, dtype=np.uint8)
-    with open('dataset/train-images.idx3-ubyte', 'rb') as imgs:
+    with open('dataset/train-images-idx3-ubyte', 'rb') as imgs:
         magic, num, nrows, ncols = struct.unpack('>IIII', imgs.read(16))
         train_images = np.fromfile(imgs, dtype=np.uint8).reshape(num, 784)
-    with open('dataset/t10k-labels.idx1-ubyte', 'rb') as labels:
+    with open('dataset/t10k-labels-idx1-ubyte', 'rb') as labels:
         magic, n = struct.unpack('>II', labels.read(8))
         test_labels = np.fromfile(labels, dtype=np.uint8)
-    with open('dataset/t10k-images.idx3-ubyte', 'rb') as imgs:
+    with open('dataset/t10k-images-idx3-ubyte', 'rb') as imgs:
         magic, num, nrows, ncols = struct.unpack('>IIII', imgs.read(16))
         test_images = np.fromfile(imgs, dtype=np.uint8).reshape(num, 784)
     return train_images, train_labels, test_images, test_labels
 
 def visualize_data(img_array, label_array):
-    
+    fig, ax = plt.subplots(nrows=8, ncols=8, sharex=True, sharey=True)
+    ax = ax.flatten()
+    for i in range(64):
+        img = img_array[label_array == 7][i].reshape(28, 28)
+        ax[i].imshow(img, cmap='Greys', interpolation='nearest')
+    plt.show()
+
+
+
+train_x, train_y, test_x, test_y = load_data()
+
+visualize_data(train_x, train_y)
