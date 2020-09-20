39a499333a622e7144c4c702c1130a5e3e8886cb
ElijahCoding <cuiyeqing960904@gmail.com>
2020-09-20 16:28:13 +0300

    transform image

--- pytorch-flask-deploy/app/torch_utils.py
+++ pytorch-flask-deploy/app/torch_utils.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torchvision.transforms as transforms
 from PIL import Image
+import io
 
 class NeuralNet(nn.Module):
     def __init__(self, input_size, hidden_size, num_classes):
@@ -26,3 +27,14 @@ model = NeuralNet(input_size, hidden_size, num_classes)
 PATH = "mnist_ffn.pth"
 model.load_state_dict(torch.load(PATH))
 model.eval()
+
+# image -> tensor
+def transform_image(image_bytes):
+    transform = transforms.Compose([transforms.Grayscale(num_output_channels=1),
+                                    transforms.Resize((28, 28)),
+                                    transforms.ToTensor(),
+                                    transforms.Normalize((0.1307,), (0.3081,))])
+
+    image = Image.open(io.BytesIO(image_bytes))
+    return transform(image).unsqueeze(0)
+
