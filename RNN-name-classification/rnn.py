7fc42680f7d2ded02924196bcd2fad95cc4c5491
ElijahCoding <cuiyeqing960904@gmail.com>
2020-09-20 17:29:35 +0300

    building rnn from scratch

---
+++ RNN-name-classification/rnn.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+import matplotlib.pyplot as plt
+
+from utils import ALL_LETTERS, N_LETTERS
+from utils import load_data, letter_to_tensor, line_to_tensor, random_training_example
+
+class RNN(nn.Module):
+    # implement RNN from scratch rather than using nn.RNN
+    def __init__(self, input_size, hidden_size, output_size):
+        super(RNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
+        self.i2o = nn.Linear(input_size + hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
