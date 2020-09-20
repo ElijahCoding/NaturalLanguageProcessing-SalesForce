59dd235c8fb2f5be1c1a6a762038def1af339a1a
ElijahCoding <cuiyeqing960904@gmail.com>
2020-09-20 19:19:21 +0300

    built

--- RNN-LSTM-GRU/main.py
+++ RNN-LSTM-GRU/main.py
@@ -50,4 +50,66 @@ class RNN(nn.Module):
         self.fc = nn.Linear(hidden_size, num_classes)
 
     def forward(self, x):
-        pass
+        # Set initial hidden states (and cell states for LSTM)
+        h0 = torch.zeros(self.num_layers, x.shape[0], self.hidden_size).to(device)
+        # x: (n, 28, 28), h0: (2, n, 128)
+
+        # forward propagate RNN
+        out, _ = self.rnn(x, h0)
+        # or:
+        # out, _ = self.lstm(x, (h0,c0))
+
+        # out: tensor of shape (batch_size, seq_length, hidden_size)
+        # out: (n, 28, 128)
+
+        # Decode the hidden state of the last time step
+        out = out[:, -1, :]
+        # out: (n, 128)
+
+        out = self.fc(out)
+        # out: (n, 10)
+        return out
+
+model = RNN(input_size, hidden_size, num_layers, num_classes).to(device)
+
+# loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+# train the model
+n_total_steps = len(train_loader)
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):
+        # origin shape: [N, 1, 28, 28]
+        # resized: [N, 28, 28]
+        images = images.reshape(-1, sequence_length, input_size).to(device)
+        labels = labels.to(device)
+
+        # forward pass
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+
+        # backward and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if (i + 1) % 100 == 0:
+            print(f'Epoch [{epoch + 1}/{num_epochs}], Step [{i + 1}/{n_total_steps}], Loss: {loss.item():.4f}')
+
+# Test the model
+# In test phase, we don't need to compute gradients (for memory efficiency)
+with torch.no_grad():
+    n_correct = 0
+    n_samples = 0
+    for images, labels in test_loader:
+        images = images.reshape(-1, sequence_length, input_size).to(device)
+        labels = labels.to(device)
+        outputs = model(images)
+        # max returns (value, index)
+        _, predicted = torch.max(outputs.data,  1)
+        n_samples += labels.shape[0]
+        n_correct += (predicted == labels).sum().item()
+
+    acc = 100.0 * n_correct / n_samples
+    print(f'Accuracy of the network on the 10000 test images: {acc} %')
