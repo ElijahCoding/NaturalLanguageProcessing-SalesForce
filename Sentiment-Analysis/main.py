78d9e89662b550418167a8cc75b1e6a5aa2f2cd0
ElijahCoding <cuiyeqing960904@gmail.com>
2020-09-22 22:20:16 +0300

    nltk beginning

--- Sentiment-Analysis/main.py
+++ Sentiment-Analysis/main.py
@@ -38,3 +38,22 @@ for word in tokenized_words:
 
 # 2) If word is present -> Add the emotion to emotion_list
 # 3) Finally count each emotion in the emotion list
+
+emotion_list = []
+with open('emotions.txt', 'r') as file:
+    for line in file:
+        clear_line = line.replace("\n", '').replace(",", '').replace("'", '').strip()
+        word, emotion = clear_line.split(':')
+
+        if word in final_words:
+            emotion_list.append(emotion)
+
+print(emotion_list)
+w = Counter(emotion_list)
+print(w)
+
+fig, ax1 = plt.subplots()
+ax1.bar(w.keys(), w.values())
+fig.autofmt_xdate()
+plt.savefig('graph.png')
+plt.show()
